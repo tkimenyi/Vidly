5d943c830c6171fe39d4edd80a74998b16f2d6fb
tkimenyi <kimthierry1@gmail.com>
2018-01-19 13:14:39 -0600

    making it RESTFul: added movies and customers controller web api controllers.

---
+++ Vidly/Controllers/api/CustomersController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //Get /api/customers
+        public IHttpActionResult GetCustomers()
+        {
+            return Ok(_context.Customers.Include(c => c.MembershipType).ToList());
+        }
+
+        // GET /api/customers/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        //POST /api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" +customer.Id), customer );
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customerInDb = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+            {
+                return NotFound();
+            }
+
+            // we can use a tool like automapper to do this mapping for us
+            customerInDb.Name = customer.Name;
+            customerInDb.BirthDate = customer.BirthDate;
+            customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerInDb.MembershipType.Name = customer.MembershipType.Name;
+            customerInDb.MembershipType.DiscountRate = customer.MembershipType.DiscountRate;
+            customerInDb.MembershipType.DurationInMonths = customer.MembershipType.DurationInMonths;
+            customerInDb.MembershipType.SignUpFee = customer.MembershipType.SignUpFee;
+
+            _context.SaveChanges();
+
+            return Ok(customerInDb);
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customerInDb);
+            return Ok(_context.SaveChanges());
+        }
+    }
+}
---
+++ Vidly/Controllers/api/MoviesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //Get /api/Movies
+        public IHttpActionResult GetMovies()
+        {
+            return Ok(_context.Movies.Include(m => m.Genre).ToList());
+        }
+
+        // GET /api/Movies/1
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+
+        //POST /api/Movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movie);
+        }
+
+        // PUT /api/Movies/1
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var movieInDb = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+            {
+                return NotFound();
+            }
+
+            // we can use a tool like automapper to do this mapping for us
+            movieInDb.Name = movie.Name;
+            movieInDb.NumberInStock = movie.NumberInStock;
+            movieInDb.ReleaseDate = movie.ReleaseDate;
+            movieInDb.DateAdded = movie.DateAdded;
+            movieInDb.Genre.Name = movie.Genre.Name;
+            _context.SaveChanges();
+
+            return Ok(movieInDb);
+        }
+
+        // DELETE /api/Movies/1
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movieInDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movieInDb);
+            return Ok(_context.SaveChanges());
+        }
+    }
+}
